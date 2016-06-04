e946b146516c0e5218e7492d12f2c1e77b0840b3
lbolanos <leonardo.bolanos@gmail.com>
2016-06-04 15:33:32 -0500

    AnsweringMachine.java

---
+++ src/main/java/com/astgspeech/services/AnsweringMachine.java
@@ -0,0 +1,78 @@
+package com.astgspeech.services;
+
+import org.asteriskjava.fastagi.AgiChannel;
+import org.asteriskjava.fastagi.AgiException;
+import org.asteriskjava.fastagi.AgiRequest;
+
+import com.astgspeech.BaseAgiRecoScript;
+import com.google.cloud.speech.v1.RecognizeResponse;
+import com.google.cloud.speech.v1.RecognizeResponse.EndpointerEvent;
+import com.google.cloud.speech.v1.SpeechRecognitionResult;
+
+public class AnsweringMachine extends BaseAgiRecoScript {
+	
+	private String lastTranscript = "";
+
+	@Override
+	public void service(AgiRequest request, AgiChannel channel) throws AgiException {		
+		/*
+        // ...say hello...
+        streamFile("welcome");
+        streamFile("tt-monkeys");
+        // ...and hangup.
+        hangup();
+        */
+		super.service(request, channel);
+	}
+
+	@Override
+	public void onError(Throwable error) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onCompleted() {
+		// TODO Auto-generated method stub		
+	}
+
+	@Override
+	public boolean onEvent(EndpointerEvent endpoint) {
+		switch( endpoint.getNumber() ) {
+			case EndpointerEvent.END_OF_SPEECH_VALUE:
+				if( lastTranscript.length() > 30 ) {
+					return false;
+				}
+				return true;
+		}
+		return true;
+	}
+
+	@Override
+	public boolean onNext(String transcript, float confidence, SpeechRecognitionResult speechRecognitionResult,
+			RecognizeResponse response) {
+		if( confidence > 0.8 ) {
+			lastTranscript = transcript;
+		}
+		try {
+			setVariable( "transcript" ,transcript );
+		} catch (AgiException e) {
+			e.printStackTrace(System.err);
+		}
+		return true;
+	}
+
+	@Override
+	public boolean onFinal(String transcript, float confidence, SpeechRecognitionResult speechRecognitionResult,
+			RecognizeResponse response) {
+		try {
+			setVariable( "transcript" ,transcript );
+		} catch (AgiException e) {
+			e.printStackTrace(System.err);
+		}
+		return false;
+	}
+
+
+
+}
